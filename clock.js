7a6a54b44e5f7c3b1512f0cc87bc76e132e72e0f
Vinicius Santana <santanaviny@gmail.com>
2017-11-23 13:21:58 -0800

    Sync time when browser tab becomes active

--- clock.js
+++ clock.js
@@ -1,25 +1,41 @@
 function Clock(el) {
-    const currentDate = new Date;
-    const hour = currentDate.getHours() % 12;
-    const minutes = currentDate.getMinutes();
-    const seconds = currentDate.getSeconds();
 
     this.el = el || document.getElementById('clock');
+    this.hourRotDegMin = -90;
+    this.minRotDegMin = -90;
+    this.secRotDegSec = -90;
 
-    // Hour hand speed (deg/min)
-    this.hourRotDegMin =  (minutes * .5) + hour * 30 - 90;
-    // Min hand speed (deg/min)
-    this.minRotDegMin = minutes * 6 - 90;
-    // Sec hand speed (deg/sec)
-    this.secRotDegSec = seconds * 6 - 90;
-
+    this.syncClock();
     this.render();
     this.runMinutes();
     this.runSeconds();
+
+
+    document.addEventListener("visibilitychange", () => {
+      // console.log(document.visibilityState);
+      if (!document.hidden) {
+        this.syncClock();
+      }
+    }, false);
+}
+
+Clock.prototype.syncClock = function() {
+  const currentDate = new Date;
+  const hour = currentDate.getHours() % 12;
+  const minutes = currentDate.getMinutes();
+  const seconds = currentDate.getSeconds();
+
+  // Hour hand speed (deg/min)
+  this.hourRotDegMin =  (minutes * .5) + hour * 30 - 90;
+  // Min hand speed (deg/min)
+  this.minRotDegMin = minutes * 6 - 90;
+  // Sec hand speed (deg/sec)
+  this.secRotDegSec = seconds * 6 - 90;
 }
 
 Clock.prototype.runMinutes = function() {
   setInterval(() => {
+    console.log('runMinutes');
     this.hourRotDegMin += .5;
     this.minRotDegMin += 3;
     this.updateClockPerMinute(this.hourRotDegMin, this.minRotDegMin);
